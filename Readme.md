01feb357f6cd447579a2bd5a7db2ddbed4a68f9f
madorb <ben.madore@slalom.com>
2014-02-17 09:42:49 -0600

    First Commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+Recreating issue as described at:
+
+http://stackoverflow.com/questions/21710450/disable-spring-boot-autoconfiguration-for-transitive-dependencies
+
+
+* gradle clean build
+** Success
+*  java -jar build/libs/boot-autoconfig-issue.jar 
+** Startup failure
+* ```java.lang.NoClassDefFoundError: org/springframework/security/web/authentication/switchuser/AuthenticationSwitchUserEvent```
+
+
+
+SecurityAutoConfiguration is disabled in app.groovy so I would expect that even though Spring Security is transitively included in runtime classpath due to dependency on Spring-WS-Sec that Spring Boot would not attempt to initialize Security.
+
+
---
+++ src/main/groovy/app.groovy
@@ -0,0 +1,28 @@
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Spring Boot "Magic" class
+ */
+@Configuration
+//The below disable appears to make no difference...
+@EnableAutoConfiguration(exclude=[SecurityAutoConfiguration.class])
+@Slf4j
+public class BaseApplicationConfig {
+
+	def static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(BaseApplicationConfig.class, args)
+
+		log.trace("All wired beans: ")
+		String[] beanNames = ctx.getBeanDefinitionNames()
+		Arrays.sort(beanNames)
+		beanNames.each { String it -> log.trace(it) }
+	}
+}
